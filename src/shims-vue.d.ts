835dc88ba3e65c21e6514b74b6b3c723d6d8fcff
龚磊 <371129453@qq.com>
2021-04-19 10:22:56 +0800

    æææ朱组生成icon组ä»,svg

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -2,7 +2,8 @@ declare module '*.vue' {
   import Vue from 'vue'
   export default Vue
 }
-// declare module "*.svg" {
-//   const content: any;
-//   export default content;
-// }
+
+declare module "*.svg" {
+  const content: string;
+  export default content;
+}
