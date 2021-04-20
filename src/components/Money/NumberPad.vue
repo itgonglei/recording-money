156e1c5c36110a84abf2c97e9ce070a28a89267d
龚磊 <371129453@qq.com>
2021-04-20 18:44:16 +0800

    NumberPad 添加数字添加删除清空等逻辑

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -1,29 +1,66 @@
 <template>
     <div class="numberPad">
-        <div class="output">100</div>
-        <div class="buttons ">
-            <button>1</button>
-            <button>2</button>
-            <button>3</button>
-            <button>删除</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>清空</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button class="ok">ok</button>
-            <button class="zero">0</button>
-            <button>.</button>
-        </div>
+      <div class="output">{{ output }}</div>
+      <div class="buttons ">
+        <button @click="inputContent">1</button>
+        <button @click="inputContent">2</button>
+        <button @click="inputContent">3</button>
+        <button @click="delIndex">删除</button>
+        <button @click="inputContent">4</button>
+        <button @click="inputContent">5</button>
+        <button @click="inputContent">6</button>
+        <button @click="clear">清空</button>
+        <button @click="inputContent">7</button>
+        <button @click="inputContent">8</button>
+        <button @click="inputContent">9</button>
+        <button @click="ok" class="ok">ok</button>
+        <button @click="inputContent" class="zero">0</button>
+        <button @click="inputContent">.</button>
+      </div>
     </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'NumberPad'
-  };
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+
+@Component
+export default class NumberPad extends Vue {
+  output: string = '0';
+
+  inputContent(event: MouseEvent) {
+    const button = (event.target as HTMLButtonElement);
+    let input = button.textContent!;
+    console.log(this.output.length);
+    if (this.output.length === 15) {
+      return;
+    }
+    if (this.output === '0') {
+      if ('0123456789'.indexOf(input) >= 0) {
+        this.output = input;
+      } else {
+        this.output += input;
+      }
+      return;
+    }
+    if (this.output.indexOf('.') >= 0 && input === '.') {return;}
+    this.output += input;
+  }
+
+  delIndex() {
+    if (this.output.length === 1) {
+      this.output = '0';
+      return;
+    }
+    this.output = this.output.slice(0, -1);
+  }
+
+  clear() {
+    this.output = '0';
+  }
+
+  ok() {}
+}
 </script>
 
 <style lang="scss" scoped>
