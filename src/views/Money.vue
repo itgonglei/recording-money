172ce49e6835de64b27d1d3c1e2cd9d304254b27
itgonglei <itgonglei@qq.com>
2021-04-30 19:12:38 +0800

    在 Money.vue 使用Tabs.vue 组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout class-prefix="layout">
     <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-    <Types :value.sync="record.type"/>
+    <Tabs :data-source="recordsTypeList" :value.sync="record.type"/>
     <FormItem filed-name="备注" placeholder="请输入备注" @update:value="onUpdateNotes"/>
     <Tags/>
   </Layout>
@@ -14,16 +14,19 @@ import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
+import recordsTypeList from '@/constants/recordsTypeList';
+import Tabs from '@/components/Tabs.vue';
 
 
 @Component({
-  components: {Tags, FormItem, Types, NumberPad},
+  components: {Tabs, Tags, FormItem, Types, NumberPad},
 })
 export default class Money extends Vue {
   get recordList() {
     return this.$store.state.recordList;
   }
 
+  recordsTypeList = recordsTypeList;
   // eslint-disable-next-line no-undef
   record: RecordItem = {
     tags: [], notes: '', type: '-', amount: 0
