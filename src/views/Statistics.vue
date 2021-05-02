aaf17c71e7eaaab6566bae28e08c315ffee2d5d2
itgonglei <itgonglei@qq.com>
2021-05-02 16:47:03 +0800

    Statistics.vue 数据列表显示

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -3,7 +3,16 @@
     <Tabs class-prefix="type" :data-source="recordsTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     <div>
-
+      <ol>
+        <li v-for="(group,index) in result" :key="index">
+          <h3>{{ group.title }}</h3>
+          <ol>
+            <li v-for="item in group.items" :key="item.id">
+              {{ item.amount }} {{ item.createdAt }}
+            </li>
+          </ol>
+        </li>
+      </ol>
     </div>
   </Layout>
 </template>
@@ -19,6 +28,29 @@ import recordsTypeList from '@/constants/recordsTypeList';
   components: {Tabs}
 })
 export default class Statistics extends Vue {
+  get recordList() {
+    return this.$store.state.recordList;
+  }
+
+  get result() {
+    const {recordList} = this;
+    // eslint-disable-next-line no-undef
+    type hashTableValue = { title: string, items: RecordItem[] };
+    // eslint-disable-next-line no-undef
+    const hashTable: { [key: string]: hashTableValue } = {};
+    for (let i = 0; i < recordList.length; i++) {
+      // eslint-disable-next-line no-unused-vars
+      const [date, time] = recordList[i].createdAt.split('T');
+      hashTable[date] = hashTable[date] || {title:date,items:[]};
+      hashTable[date].items.push(recordList[i]);
+    }
+    return hashTable;
+  }
+
+  beforeCreate() {
+    this.$store.commit('fetchRecords');
+  }
+
   type = '-';
   interval = 'day';
   intervalList = intervalList;
