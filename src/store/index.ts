7fd0cfec01f863b976a702a4cd553b50911992b5
龚磊 <371129453@qq.com>
2021-05-08 17:23:03 +0800

    修改部分bug 删除了alert

--- src/store/index.ts
+++ src/store/index.ts
@@ -77,14 +77,12 @@ const store = new Vuex.Store({
             state.createTagError = null;
             const names = state.tagList.map(item => item.name);
             if (names.indexOf(name) >= 0) {
-                window.alert('标签名重复了');
                 state.createTagError = new Error('tag name duplicated');
                 return;
             }
             const id = createId().toString();
             state.tagList.push({id, name: name});
             store.commit('saveTags');
-            window.alert('添加成功');
         },
         saveTags(state) {
             window.localStorage.setItem('tagList', JSON.stringify(state.tagList));
